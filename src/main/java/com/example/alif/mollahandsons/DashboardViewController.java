62bc46e8b55a74bef89c94e4a84de5c79e4795b4
Shahriar Alif <fahim.shahriar.alif@gmail.com>
2025-07-31 01:11:37 +0600

    Fixed the dashboard controller class

--- src/main/java/com/example/alif/mollahandsons/DashboardViewController.java
+++ src/main/java/com/example/alif/mollahandsons/DashboardViewController.java
@@ -1,4 +1,35 @@
 package com.example.alif.mollahandsons;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableView;
+
 public class DashboardViewController {
+
+
+    public void signOutButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    public void addCompanyButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    public void addProductButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    public void viewInventoryButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    public void addResellerButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    public void makeSaleButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    public void salesHistoryButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    public void revenueReportButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    public void resellerListButtonOnAction(ActionEvent actionEvent) {
+    }
 }
