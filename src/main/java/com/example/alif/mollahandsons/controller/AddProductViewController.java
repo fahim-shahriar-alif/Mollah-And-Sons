015e91b9df2a152ad84725f6f3d193eda2828099
Shahriar Alif <fahim.shahriar.alif@gmail.com>
2025-08-01 00:50:43 +0600

    Added the add product controller and created the methods

--- src/main/java/com/example/alif/mollahandsons/controller/AddProductViewController.java
+++ src/main/java/com/example/alif/mollahandsons/controller/AddProductViewController.java
@@ -1,4 +1,31 @@
 package com.example.alif.mollahandsons.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class AddProductViewController {
+    public ComboBox companyComboBox;
+    public TextField nameTextField;
+    public TextField brandTextField;
+    public ComboBox typeComboBox;
+    public ComboBox categoryComboBox;
+    public TextField unitPriceTextField;
+    public TextField costPriceTextField;
+    public TextField stockQuantityTextField;
+    public TextField minStockLevelTextField;
+    public ComboBox unitComboBox;
+    public TextArea descriptionAreaField;
+
+
+    public void saveButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    public void clearButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    public void cancelButtonOnAction(ActionEvent actionEvent) {
+    }
 }
