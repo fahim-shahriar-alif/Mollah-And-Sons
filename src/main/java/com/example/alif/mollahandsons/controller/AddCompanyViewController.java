dc20a6951635904ae5267452fbd18ee973d99a04
Shahriar Alif <fahim.shahriar.alif@gmail.com>
2025-08-01 00:21:31 +0600

    Added New Company Section

---
+++ src/main/java/com/example/alif/mollahandsons/controller/AddCompanyViewController.java
@@ -0,0 +1,29 @@
+package com.example.alif.mollahandsons.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class AddCompanyViewController {
+    public TextField companyNameTextField;
+    public TextArea addressTextArea;
+    public TextField contactPersonTextField;
+    public TextField phoneNumberTextField;
+    public TextField emailTextField;
+    public TextField cityTextField;
+    public TextField bankAccounttextField;
+    public ComboBox companyTypeComboBox;
+
+    public void backButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    public void saveCompanyButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    public void clearFormButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    public void cancelButtonOnAction(ActionEvent actionEvent) {
+    }
+}
